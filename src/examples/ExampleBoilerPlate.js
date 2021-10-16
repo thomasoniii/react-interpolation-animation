beb7b1c2230ef0c87d4e62fa1ecbdbebe1f898fd
Jim Thomason <jim@japanesewartuba.com>
2021-10-16 12:46:09 -0500

    capitalized code by default

--- src/examples/ExampleBoilerPlate.js
+++ src/examples/ExampleBoilerPlate.js
@@ -2,10 +2,12 @@ import React from "react"
 import ExampleFrame from "./ExampleFrame"
 
 const blurb = "SOME BLURB"
-const code = () => {}
+const Code = () => {
+  return null
+}
 
 const ExampleNUMBER = () => {
-  return <ExampleFrame blurb={blurb} code={code} />
+  return <ExampleFrame blurb={blurb} code={Code} />
 }
 
 export default ExampleNUMBER
